e90f4d47ddf9ad0b0adacb581bd2b78d4555c965
Kevin Janisch <106290@fhwn.ac.at>
2019-10-21 13:28:48 +0200

    added mocha

---
+++ test/test.js
@@ -0,0 +1,22 @@
+var request = require("request"),
+    assert = require('assert'),
+    chai = require = ('chai'),
+    base_url = 'https://fhwn19-node-08.azurewebsites.net/'
+
+describe("Node Express", function() {
+    describe("GET /", function() {
+        it ("returns status code 200", function(done){
+            request.get(base_url, function(error, response, body) {
+                assert.equal(200, response.statusCode);
+                done();
+            });
+        });
+
+        it("Contains Welcome", function(done){
+            request.get(base_url, function(error, response, body){
+                chai.assert.include(body, "Welcome");
+                done();
+            });
+        });
+    });
+});
